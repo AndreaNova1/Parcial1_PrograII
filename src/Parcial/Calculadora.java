b305e18840d80902416d3dbd1c3c49ecb3df392c
AndreaNova1 <anoval@miumg.edu.gt>
2021-08-08 11:18:33 -0600

    se actualizo la clase calculadora y sus metodos

--- src/Parcial/Calculadora.java
+++ src/Parcial/Calculadora.java
@@ -1,46 +1,67 @@
 package Parcial;
 import javax.swing.*;
-
+import java.util.Random;
+import java.util.Scanner;
 public class Calculadora {
 
-    private int operacion1;
-    private int operacion2;
+    double numero1;
+    double numero2;
     double resultado;
 
-    //Constructor
-    public Calculadora() {
-        operacion1 = 0;
-        operacion2 = 0;
-        resultado = 0;
-    }
 
     //metodos
     public void suma() {
-        operacion1 = Integer.parseInt(JOptionPane.showInputDialog("Digite el primer valor:"));
-        operacion2 = Integer.parseInt(JOptionPane.showInputDialog("Digite el segundo valor:"));
-        resultado = operacion1 + operacion2;
-        JOptionPane.showInputDialog("El resultado es" + resultado);
+        Scanner ingresar = new Scanner(System.in);
+
+        System.out.println("Ingrese el primer numero");
+        numero1=ingresar.nextDouble();
+
+        System.out.println("Ingrese el segundo numero");
+        numero2=ingresar.nextDouble();
+
+        resultado=numero1 + numero2;//contatenacion
+        System.out.println("El resultado de la suma es" + resultado);
+
+        }
+
+    public void resta(){
+            Scanner ingresar = new Scanner(System.in);
+
+            System.out.println("Ingrese el primer numero");
+            numero1=ingresar.nextDouble();
+
+            System.out.println("Ingrese el segundo numero");
+            numero2=ingresar.nextDouble();
+        resultado=numero1 - numero2;//contatenacion
+        System.out.println("El resultado de la resta es" + resultado);
+
+        }
+    public void multiplicacion(){
+            Scanner ingresar = new Scanner(System.in);
+
+            System.out.println("Ingrese el primer numero");
+            numero1=ingresar.nextDouble();
+
+            System.out.println("Ingrese el segundo numero");
+            numero2=ingresar.nextDouble();
+            resultado=numero1 * numero2;//contatenacion
+            System.out.println("El resultado de la multiplicacion es" + resultado);
+
+        }
+    public void division (){
+        Scanner ingresar = new Scanner(System.in);
+
+        System.out.println("Ingrese el primer numero");
+        numero1=ingresar.nextDouble();
+
+        System.out.println("Ingrese el segundo numero");
+        numero2=ingresar.nextDouble();
+        resultado=numero1 / numero2;//contatenacion
+        System.out.println("El resultado de la suma es" + resultado);
+
+    }
+    public void mostrar(){
 
     }
 
-    public void resta() {
-        operacion1 = Integer.parseInt(JOptionPane.showInputDialog("Digite el primer valor:"));
-        operacion2 = Integer.parseInt(JOptionPane.showInputDialog("Digite el segundo valor:"));
-        resultado = operacion1 - operacion2;
-        JOptionPane.showInputDialog("El resultado es" + resultado);
-    }
-
-    public void multiplicacion () {
-        operacion1 = Integer.parseInt(JOptionPane.showInputDialog("Digite el primer valor:"));
-        operacion2 = Integer.parseInt(JOptionPane.showInputDialog("Digite el segundo valor:"));
-        resultado = operacion1 * operacion2;
-        JOptionPane.showInputDialog("El resultado es" + resultado);
-    }
-
-    public void division() {
-        operacion1 = Integer.parseInt(JOptionPane.showInputDialog("Digite el primer valor:"));
-        operacion2 = Integer.parseInt(JOptionPane.showInputDialog("Digite el segundo valor:"));
-        resultado = operacion1 / operacion2;
-        JOptionPane.showInputDialog("El resultado es" + resultado);
-    }
 }
--- src/Parcial/Main.java
+++ src/Parcial/Main.java
@@ -1,38 +1,46 @@
 package Parcial;
-
 import java.util.Scanner;
 //clase principal
 public class Main {
+    static Calculadora calculadora = new Calculadora();
 // menu
-    public static void menu(){
-        Scanner entrada = new Scanner(System.in);
-        int op;
-        do {
-            System.out.printf("\n\n");
-            System.out.println("Bienvenido al menu de opciones");
-            System.out.println("1) Calculadora Basica");
-            System.out.println("2) Calculadora Avanzada");
-            System.out.println("3) Salir del Sistema");
-            System.out.println("Ingresa una Opcion:"); op = entrada.nextInt();
-
-            switch (op){//casos que se pueden realizar
-                case 1:
-                    System.out.printf("Bienvenido a la calculadora Basica");
-                    break;
-                case 2:
-                    System.out.printf("Bienvenido a la calculadora Avanzada");
-                    break;
-                case 3:
-                    System.out.printf("Fin del programa");
-                    System.exit(0);
-                    break;
-                default:
-                    System.out.println("Ingresa una opcion valida del menu");
-                              }
-        }while (op !=4);
-    }
+public static void menu() {
+    Scanner entrada = new Scanner(System.in);
+    int op;
+    do {
+        System.out.printf("\n\n");
+        System.out.println("Bienvenido al menu de opciones");
+        System.out.println("Calculadora de operaciones basicas");
+        System.out.println("1) suma");
+        System.out.println("2) resta");
+        System.out.println("3) multiplicacion");
+        System.out.println("4) division");
+        System.out.println("Ingresa una Opcion:");
+        op = entrada.nextInt();
+        switch (op) {
+            case 1:
+                calculadora.suma();
+                break;
+            case 2:
+                calculadora.resta();
+                break;
+            case 3:
+                calculadora.multiplicacion();
+                break;
+            case 4:
+                calculadora.division();
+                break;
+            case 5:
+                System.out.println("Hasta pronto....");
+                System.exit(0);
+                break;
+            default:
+                System.out.println("Ingrese un número correcto segun las opciones");
+                break;
+        }
+    } while (op != 5);
+}
     public static void main(String[] args){
         menu();
-
-    }
+}
 }
