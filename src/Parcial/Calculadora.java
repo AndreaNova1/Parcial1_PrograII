bfd1cd877e2a64cf96ae5865264f2cb997cfd42d
AndreaNova1 <anoval@miumg.edu.gt>
2021-08-08 00:04:03 -0600

    se ha agregado un menu

--- src/Parcial/Calculadora.java
+++ src/Parcial/Calculadora.java
@@ -8,19 +8,40 @@ public class Calculadora {
     private int operacion2;
     double resultado;
 
-   //Constructor
-public Calculadora(){
-    operacion1=0;
-    operacion2=0;
-    resultado=0;
-}
-//metodos
-    public void suma(){
-    operacion1=Integer.parseInt(JOptionPane.showInputDialog("Digite el primer valor:"));
+    //Constructor
+    public Calculadora() {
+        operacion1 = 0;
+        operacion2 = 0;
+        resultado = 0;
+    }
+
+    //metodos
+    public void suma() {
+        operacion1 = Integer.parseInt(JOptionPane.showInputDialog("Digite el primer valor:"));
+        operacion2 = Integer.parseInt(JOptionPane.showInputDialog("Digite el segundo valor:"));
+        resultado = operacion1 + operacion2;
+        JOptionPane.showInputDialog("El resultado es" + resultado);
 
     }
-    public void resta(){
-        System.out.println("prueba");
 
+    public void resta() {
+        operacion1 = Integer.parseInt(JOptionPane.showInputDialog("Digite el primer valor:"));
+        operacion2 = Integer.parseInt(JOptionPane.showInputDialog("Digite el segundo valor:"));
+        resultado = operacion1 - operacion2;
+        JOptionPane.showInputDialog("El resultado es" + resultado);
+    }
+
+    public void multiplicacion() {
+        operacion1 = Integer.parseInt(JOptionPane.showInputDialog("Digite el primer valor:"));
+        operacion2 = Integer.parseInt(JOptionPane.showInputDialog("Digite el segundo valor:"));
+        resultado = operacion1 * operacion2;
+        JOptionPane.showInputDialog("El resultado es" + resultado);
+    }
+
+    public void division() {
+        operacion1 = Integer.parseInt(JOptionPane.showInputDialog("Digite el primer valor:"));
+        operacion2 = Integer.parseInt(JOptionPane.showInputDialog("Digite el segundo valor:"));
+        resultado = operacion1 / operacion2;
+        JOptionPane.showInputDialog("El resultado es" + resultado);
     }
 }
--- src/Parcial/Main.java
+++ src/Parcial/Main.java
@@ -1,5 +1,45 @@
 package Parcial;
 
+import java.util.Scanner;
+
 public class Main {
+
+    public static void menu(){
+        Scanner entrada = new Scanner(System.in);
+        int op;
+        do {
+            System.out.printf("\n\n");
+            System.out.println("Bienvenido al menu de opciones");
+            System.out.println("1) Calculadora Basica");
+            System.out.println("2) Calculadora Avanzada");
+            System.out.println("3) Salir del Sistema");
+            System.out.println("Ingresa una Opcion:"); op = entrada.nextInt();
+
+            switch (op){
+                case 1:
+                    System.out.printf("Bienvenido a la calculadora Basica");
+                    break;
+                case 2:
+                    System.out.printf("Bienvenido a la calculadora Avanzada");
+                    break;
+                case 3:
+                    System.out.printf("Hasta pronto");
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Ingresa una opcion valida del menu");
+                              }
+
+        }while (op !=4);
+
+
+    }
+
+    public static void main(String[] args){
+
+        menu();
+
+
+    }
 
 }
