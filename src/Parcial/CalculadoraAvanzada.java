8f440803431281b2e4a80a59687597d93d56d782
AndreaNova1 <anoval@miumg.edu.gt>
2021-08-08 20:05:18 -0600

    se incluyo el metodo del seno

--- src/Parcial/CalculadoraAvanzada.java
+++ src/Parcial/CalculadoraAvanzada.java
@@ -21,7 +21,8 @@ public class CalculadoraAvanzada {
 
         resultado = Math.pow(numero1, numero2);
         System.out.println("La potencia obtenida es  " + resultado);
-    }
+        
+        }
         public void Raiz () {
         System.out.println("Operacion de radicacion");
         Scanner mostrar=new Scanner(System.in);
@@ -32,10 +33,22 @@ public class CalculadoraAvanzada {
 
         }
         public void Seno () {
+                System.out.println("Calculo del Seno de un triangulo rectangulo");
+                Scanner mostrar = new Scanner(System.in);
 
-        }
+                System.out.println("Ingrese el valor del cateto opuesto");
+                numero1 =mostrar.nextDouble();
+
+                System.out.println("Ingrese el valor de la hipotenusa");
+                numero2 =mostrar.nextDouble();
+
+                double  rad=Math.toRadians(numero1/numero2);
+                resultado=Math.sin(rad);
+
+                System.out.println("El seno del un numero "+numero1+numero2+"º es: "+resultado);
+                System.out.println(" ");
+            }
         public void Coseno () {
 
         }
     }
-
